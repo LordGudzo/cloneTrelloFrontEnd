5d6a1d32b75617ff7db7d11870a5770c186c0c49
LordGudzo <lordgudzo@ukr.net>
2023-05-07 22:30:16 +0300

    add prettier,eslint, husky,lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "useTabs": false,           // sets the use of spaces instead of tab characters
+    "semi": true,               // requires semicolons at the end of statements
+    "trailingComma": "es5",     // adds a trailing comma when it's valid in ES5 (but not in ES6)
+    "bracketSpacing": true,     // spaces between the opening and closing brackets of an object or array.
+    "singleQuote": true,        // enforces the use of single quotes for string literals
+    "printWidth": 120,          // specifies the line length that the printer will wrap on
+    "tabWidth": 2               // sets the number of spaces per indentation-level for code blocks
+}
